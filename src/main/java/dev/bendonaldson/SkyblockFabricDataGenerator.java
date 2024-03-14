d141a75e13a3ff2a84d7bc9a7d47cbb1126e2d29
Ben <benjamin@hoplite.gg>
2024-03-14 05:13:15 -0700

    Initial Template

---
+++ src/main/java/dev/bendonaldson/SkyblockFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.bendonaldson;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyblockFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
