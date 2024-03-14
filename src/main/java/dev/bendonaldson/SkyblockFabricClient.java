a2fc1b9faf201d21d8a2903645d07739eafbc0b0
Ben <benjamin@hoplite.gg>
2024-03-14 05:24:37 -0700

    Initial Client

---
+++ src/main/java/dev/bendonaldson/SkyblockFabricClient.java
@@ -0,0 +1,10 @@
+package dev.bendonaldson;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyblockFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
